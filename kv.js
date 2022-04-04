27247705e0f4d9774ee37f9e052c9e3d435d872d
alperr <alper3017@gmail.com>
2022-04-04 23:04:09 +0300

    initial commit

---
+++ kv.js
@@ -0,0 +1,107 @@
+const SQLITE = require("sqlite");
+const SCHEMA = {"data": {"columns": ["k", "v"], "index": [], "primary": "k" }}
+const FLUSH_CHECK = 60 * 1000;
+const MEMORY_CLEAR_FREQUENCY = 10 * 60 * 1000;
+
+class KV
+{
+	constructor(path, is_object)
+	{
+		var _this = this;
+		this.is_object = false;
+		if (typeof is_object == "undefined");
+			this.is_object = true;
+
+		this.cache = {};
+		this.last_operation = new Map();
+		this.dirty = new Set();
+		this.deleted = new Set();
+
+		this.db = new SQLITE(path, SCHEMA);
+		setInterval(check, FLUSH_CHECK);
+		
+		function check()
+		{
+			_this.flush();
+			_this.remove_infrequent_keys();
+		}
+	}
+
+	set(k, v)
+	{
+		if (this.deleted.has(k))
+			this.deleted.delete(k);
+
+		this.last_operation.set(k, Date.now());
+		this.dirty.add(k);
+		this.cache[k] = v;
+	}
+
+	get(k)
+	{
+		if (this.deleted.has(k))
+			return undefined;
+
+		this.last_operation.set(k, Date.now());
+		var v = this.cache[k]
+		if (typeof v == "undefined")
+			v = this.db.read("SELECT v FROM data WHERE k = ?", k)[0];	
+
+		if (typeof v == "undefined")
+			return undefined;
+
+		if (this.is_object)
+			v = JSON.parse(v);
+
+		this.cache[k] = v;
+		return v;
+	}
+
+	del(k)
+	{
+		this.deleted.add(k);
+	}
+
+	flush()
+	{
+		var _this = this;
+		this.deleted.forEach(function(k)
+		{
+			delete _this.cache[k];
+			_this.last_operation.delete(k);
+			_this.dirty.delete(k);
+			_this.db.fast_write("DELETE FROM data WHERE k = ?", k);
+		});
+
+		this.dirty.forEach(function(k)
+		{
+			var params = [k];
+			if (_this.is_object)
+				params.push(JSON.stringify(_this.cache[k]));
+			else
+				params.push(_this.cache[k]);
+
+			_this.db.fast_write("REPLACE INTO data (k,v) VALUES (?,?)", params);
+		})
+
+		this.db.commit();
+		this.dirty = new Set();
+		this.deleted = new Set();
+	}
+
+	remove_infrequent_keys()
+	{
+		var _this = this;
+		var now = Date.now();
+		this.last_operation.forEach(function(v, k)
+		{
+			if (now - v > MEMORY_CLEAR_FREQUENCY)
+			{
+				_this.last_operation.delete(k);
+				delete _this.cache[k];
+			}
+		});
+	}
+}
+
+module.exports = KV;
